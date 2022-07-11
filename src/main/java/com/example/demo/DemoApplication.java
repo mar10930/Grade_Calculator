3261130c68d997d3d9b2a670c1a3eeb84927ce98
marioluja <mario4education@gmail.com>
2022-07-11 16:30:25 -0700

    Created a connection between the database and the backend. Also added CRUD functionality.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,6 @@ package com.example.demo;
 import com.example.demo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @SpringBootApplication
@@ -14,14 +11,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-	}
-	//Method serve as rest endpoint
-	@GetMapping
-	public List<Student> hello()
-	{
-		return List.of(
-				new Student("mluja1@asu.edu","abcd", "Mario","Luja","CS")
-		);
 	}
 
 }
